013dc90cd656980ff4755f8f66f9bf87cb2a1b26
aubertcrm <daraienzo@gmail.com>
2026-08-31 17:15:44 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Fiches d'intervention — CRM</title>
+        <title>AUBERT CRM</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1C2321" />
+        <meta name="theme-color" content="#150E0B" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
